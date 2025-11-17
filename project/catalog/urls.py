6782f6be7f8c7249d832fc74e3dd252cc3cf9788
Olena Kysilova <e.a.kiseljova@gmail.com>
2025-11-17 19:23:24 +0200

    Add pages index, product, category with base data

--- project/catalog/urls.py
+++ project/catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import catalog_page, product_page
+from .views import catalog_page, product_page, category_page
 
 urlpatterns = [
-    path('', catalog_page),
-    path('product/', product_page),
+    path("", catalog_page),
+    path("category/<str:slug>", category_page, name="category_page"),
+    path("product/<str:slug>", product_page, name="product_page"),
 ]
--- project/catalog/views.py
+++ project/catalog/views.py
@@ -1,9 +1,32 @@
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from .data import PRODUCTS, CATEGORIES
 
 
 def catalog_page(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('<html><body><h1>Catalog Page</h1></body></html>')
+    chunk_size = 3
+    rows = [PRODUCTS[i : i + chunk_size] for i in range(0, len(PRODUCTS), chunk_size)]
+
+    context = {"page_name": "Catalog", "rows": rows, "categories": CATEGORIES}
+    return render(request=request, template_name="catalog/index.html", context=context)
 
 
-def product_page(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('<html><body><h1>Product Page</h1></body></html>')
+def product_page(request: HttpRequest, slug) -> HttpResponse:
+    products = [p for p in PRODUCTS if p["slug"] == slug]
+    product = products[0]
+
+    context = {"page_name": product["name"], "product": products[0]}
+    return render(
+        request=request, template_name="catalog/product.html", context=context
+    )
+
+
+def category_page(request: HttpRequest, slug) -> HttpResponse:
+    categories = [c for c in CATEGORIES if c["slug"] == slug]
+    category = categories[0]
+
+    context = {"page_name": category["name"], "category": categories[0]}
+    return render(
+        request=request, template_name="catalog/category.html", context=context
+    )
