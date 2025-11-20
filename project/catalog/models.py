8e4c3968fed14b84de765940222b3e81499f9b89
Olena Kysilova <e.a.kiseljova@gmail.com>
2025-11-20 16:37:31 +0200

    Add Category and Product models

--- project/catalog/models.py
+++ project/catalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    image = models.ImageField(upload_to="products/", null=True, blank=True)
+    price = models.FloatField()
+    sale = models.DecimalField(default=0, decimal_places=2, max_digits=3)
+    in_stock = models.BooleanField(null=True)
+    description = models.TextField(null=True, blank=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    products = models.ManyToManyField(
+        Product,
+        related_name="categories",
+    )
