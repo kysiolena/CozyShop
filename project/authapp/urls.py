dfa9255e4ebafc67a2a0b450a3d558d078217835
Olena Kysilova <e.a.kiseljova@gmail.com>
2025-12-18 21:16:30 +0200

    Complete base authentication in the AuthApp

--- project/authapp/urls.py
+++ project/authapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from authapp.views import (
     UpdatePasswordView,
-    ProfileView,
+    ProfileUpdateView,
     SignOutView,
     SignUpView,
     SignInView,
@@ -12,6 +12,6 @@ urlpatterns = [
     path("sign-in/", SignInView.as_view(), name="sign_in_page"),
     path("sign-up/", SignUpView.as_view(), name="sign_up_page"),
     path("sign-out/", SignOutView.as_view(), name="sign_out_page"),
-    path("profile/", ProfileView.as_view(), name="profile_page"),
+    path("profile/", ProfileUpdateView.as_view(), name="profile_page"),
     path("update-password/", UpdatePasswordView.as_view(), name="update_password_page"),
 ]
--- project/authapp/views.py
+++ project/authapp/views.py
@@ -1,23 +1,106 @@
-from django.contrib.auth import views as auth_views
-from django.views.generic import TemplateView
-from django.views.generic.edit import CreateView
+from django.contrib import messages
+from django.contrib.auth import views as auth_views, get_user_model
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView
+
+from authapp.forms import SignUpForm, SignInForm, ProfileUpdateForm, PasswordChangeForm
+from shop.views import BaseContextMixin
+
+# Get User Current Model
+UserModel = get_user_model()
 
 
-class SignInView(auth_views.LoginView):
+class RedirectAuthenticatedUserMixin:
+    """Redirects authenticated users away from 'Anonymous Only' pages."""
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            messages.info(
+                self.request,
+                "You already logged in.",
+            )
+
+            # Redirect to profile page
+            return redirect("profile_page")
+
+        return super().dispatch(request, *args, **kwargs)
+
+
+class RedirectNoAuthenticatedUserMixin:
+    """Redirects no authenticated users away from 'Private Only' pages."""
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            messages.error(
+                self.request,
+                f"You must be authenticated to visit the {self.page_name or ""} page.",
+            )
+
+            # Redirect to login page
+            return redirect("sign_in_page")
+
+        return super().dispatch(request, *args, **kwargs)
+
+
+class SignInView(
+    RedirectAuthenticatedUserMixin, BaseContextMixin, auth_views.LoginView
+):
     template_name = "authapp/sign-in.html"
+    page_name = "Sign In"
+    form_class = SignInForm
+    next_page = "profile_page"
 
 
-class SignUpView(CreateView):
+class SignUpView(RedirectAuthenticatedUserMixin, BaseContextMixin, CreateView):
     template_name = "authapp/sign-up.html"
+    page_name = "Sign Up"
+    form_class = SignUpForm
+    success_url = reverse_lazy("profile_page")
 
 
-class SignOutView(auth_views.LogoutView):
-    template_name = "authapp/sign-out.html"
+class SignOutView(RedirectNoAuthenticatedUserMixin, auth_views.LogoutView):
+    next_page = "sign_in_page"
+
+    def get_success_url(self):
+        next_page = super().get_success_url()
+
+        messages.success(self.request, "You have been successfully logged out!")
+
+        if next_page:
+            # If next_page is defined, add the message before redirecting
+            return next_page
+
+        # If no next_page is defined, redirect to a default page (e.g., home)
+        # and display the message.
+        return reverse_lazy("shop_page")
 
 
-class ProfileView(TemplateView):
-    template_name = "authapp/profile.html"
-
-
-class UpdatePasswordView(auth_views.PasswordChangeView):
+class UpdatePasswordView(
+    SuccessMessageMixin,
+    RedirectNoAuthenticatedUserMixin,
+    BaseContextMixin,
+    auth_views.PasswordChangeView,
+):
     template_name = "authapp/update-password.html"
+    page_name = "Update Password"
+    success_url = reverse_lazy("profile_page")
+    success_message = "Your password was changed successfully!"
+    form_class = PasswordChangeForm
+
+
+class ProfileUpdateView(RedirectNoAuthenticatedUserMixin, BaseContextMixin, UpdateView):
+    template_name = "authapp/profile.html"
+    page_name = "Profile"
+    form_class = ProfileUpdateForm
+    success_url = reverse_lazy("profile_page")
+
+    def get_object(self, queryset=None):
+        """
+        Returns the object the view is displaying.
+        In this case, the Profile related to the currently logged-in user. (To Do)
+        """
+
+        # Ensure the user is logged in to access their own profile
+        return self.request.user
