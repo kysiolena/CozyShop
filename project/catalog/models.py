60ea83f9900bc32701f9900221be48e566f58b21
Olena Kysilova <e.a.kiseljova@gmail.com>
2025-12-21 09:29:27 +0200

    Create Profile model in AuthApp

--- project/catalog/models.py
+++ project/catalog/models.py
@@ -1,15 +1,7 @@
 from django.conf import settings
 from django.db import models
 
-
-class TimeStampedModel(models.Model):
-    """Abstract base class that adds created_at and updated_at fields to models."""
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
+from shop.models import TimeStampedModel
 
 
 class Category(TimeStampedModel):
