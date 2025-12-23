5395d75642cc702415fe31acef3fed0c9c53af1f
Olena Kysilova <e.a.kiseljova@gmail.com>
2025-12-23 14:07:55 +0200

    Create logic for saving local Cart to cart_temporary field of Profile model

--- project/cart/models.py
+++ project/cart/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.signals import user_logged_in
 
-# Create your models here.
+from cart.services import Cart
+
+UserModel = get_user_model()
+
+
+# Sync local Cart with Profile Cart when User is logging in
+def sync_cart(sender, request, user, **kwargs):
+    # Get Cart
+    cart = Cart(request)
+
+    # Sync local Cart and Profile Cart
+    cart.sync()
+
+
+user_logged_in.connect(sync_cart, sender=UserModel)
--- project/cart/services.py
+++ project/cart/services.py
@@ -16,6 +16,7 @@ class Cart:
 
     def __init__(self, request) -> None:
         self._session = request.session
+        self._request = request
 
         # If cart doesn't exist yet
         if self._key not in self._session:
@@ -28,30 +29,50 @@ class Cart:
         self._cart = cart
 
     def get_items(self) -> list[TCartItem]:
+        """Get items Cart from session."""
         return self._cart
 
     def get_ids(self) -> list[int]:
+        """Get products ids from the Cart"""
         return [item["product_id"] for item in self._cart]
 
     def get_item(self, product_id: int) -> TCartItem | None:
+        """Get item by product id from the Cart"""
         try:
-            return [item for item in self._cart if item["product_id"] == product_id][0]
+            filtered_items = [
+                item for item in self._cart if item["product_id"] == product_id
+            ]
+
+            if len(filtered_items):
+                return filtered_items[0]
+            else:
+                raise IndexError
         except Exception as e:
             logger.error(f"Failed to get Product #{product_id} from the Cart: {e}")
 
             return None
 
     def get_item_index(self, product_id: int) -> int:
+        """Get item index by product id from the Cart"""
         try:
             item = self.get_item(product_id)
 
-            return self._cart.index(item)
+            if item is not None:
+                return self._cart.index(item)
+            else:
+                raise IndexError
         except Exception as e:
             logger.error(f"Failed to get Product #{product_id} index in the Cart: {e}")
 
             return -1
 
     def add_item(self, product_id: int, quantity: int) -> bool:
+        """
+        Add an item to the cart.
+        :param product_id:
+        :param quantity:
+        :return:
+        """
         try:
             # Product already in the cart
             if product_id in self.get_ids():
@@ -62,47 +83,167 @@ class Cart:
             # Update Cart
             self._cart.append(new_item)
 
-            self._session.modified = True
+            # Sync local Cart and Profile Cart
+            success = self.sync()
 
-            return True
+            if success:
+                return True
+            else:
+                raise Exception("Failed update Profile cart")
         except Exception as e:
             logger.error(f"Failed to add Product #{product_id} to the Cart: {e}")
 
             return False
 
     def delete_item(self, product_id: int) -> bool:
+        """
+        Delete an item from the cart.
+        :param product_id:
+        :return:
+        """
         try:
             # Update cart
             item_index = self.get_item_index(product_id)
 
             del self._cart[item_index]
 
-            self._session.modified = True
+            # Sync local Cart and Profile Cart
+            success = self.sync([product_id])
 
-            return True
+            if success:
+                return True
+            else:
+                raise Exception("Failed update Profile cart")
         except Exception as e:
             logger.error(f"Failed to delete Product #{product_id} from the Cart: {e}")
 
             return False
 
     def update(self, data: list[TCartItem]) -> bool:
+        """
+        Update the cart.
+        :param data:
+        :return:
+        """
         try:
-            # Update cart
-            self._cart = self._session[self._key] = data
+            # Set key of Session Cart with new value
+            self._cart = data
 
-            return True
+            # Sync local Cart and Profile Cart
+            success = self.sync()
+
+            if success:
+                return True
+            else:
+                raise Exception("Failed update Profile cart")
         except Exception as e:
             logger.error(f"Failed to update Cart: {e}")
 
             return False
 
     def clean(self) -> bool:
+        """
+        Clean the cart.
+        :return:
+        """
         try:
-            self._cart = self._session[self._key] = []
+            # Get ids products of Cart
+            ids = self.get_ids()
 
-            return True
+            # Clean Cart
+            self._cart = []
+
+            # Sync local Cart and Profile Cart
+            success = self.sync(ids_for_delete=ids)
+
+            if success:
+                return True
+            else:
+                raise Exception("Failed update Profile cart")
         except Exception as e:
             logger.error(f"Failed to clean the Cart: {e}")
+
+            return False
+
+    def get_items_from_profile(self) -> list[TCartItem]:
+        """Get Cart value from profile model"""
+        try:
+            # Get User
+            user = self._request.user
+
+            if user.is_authenticated:
+                # Get Profile
+                profile = user.profile
+
+                profile_cart_json = profile.cart_temporary
+
+                if profile_cart_json:
+                    return json.loads(profile_cart_json)
+                else:
+                    raise Exception("Field cart_temporary is blank or NULL")
+            else:
+                raise Exception("User is not authenticated")
+        except Exception as e:
+            logger.error(f"Failed to get Cart from Profile: {e}")
+
+            return []
+
+    def set_items_to_profile(self, data: list[TCartItem]) -> bool:
+        """Set Cart value to profile model"""
+        try:
+            # Get User
+            user = self._request.user
+
+            if user.is_authenticated:
+                # Get Profile
+                profile = user.profile
+
+                data_json = json.dumps(data)
+
+                # Save new value
+                profile.cart_temporary = data_json
+                profile.save()
+
+                return True
+            else:
+                raise Exception("User is not authenticated")
+        except Exception as e:
+            logger.error(f"Failed to set items to profile from the Cart: {e}")
+
+            return False
+
+    def sync(self, ids_for_delete: list[int] | None = None) -> bool:
+        """
+        Compare the local version and the profile version of the Cart, then save the final version to the profile.
+        :param ids_for_delete:
+        :return:
+        """
+        try:
+            # Get cart from Profile
+            profile_cart = self.get_items_from_profile()
+
+            # Local cart
+            local_cart = self.get_items()
+
+            # Compare two carts
+            for p_c_i in profile_cart:
+                l_c_i = self.get_item(p_c_i["product_id"])
+
+                if not l_c_i and p_c_i["product_id"] not in (ids_for_delete or []):
+                    local_cart.append(p_c_i)
+
+            # Update Profile cart
+            success = self.set_items_to_profile(local_cart)
+
+            if success:
+                # Set new value for key of Session Cart
+                self._cart = self._session[self._key] = local_cart
+
+                return True
+            else:
+                raise Exception("Failed update Profile cart")
+        except Exception as e:
+            logger.error(f"Failed to sync local Cart with Profile Cart: {e}")
 
             return False
 
