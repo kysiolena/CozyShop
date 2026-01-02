89dcd30e2805aef4c0eb3d6bc00e31e91bcc9c81
Olena Kysilova <e.a.kiseljova@gmail.com>
2026-01-02 12:42:26 +0200

    Add breadcrumbs.html in ShopApp

--- project/shop/views.py
+++ project/shop/views.py
@@ -4,6 +4,13 @@ from django.views.generic import TemplateView
 class BaseContextMixin:
     page_name = "Page Title"
 
+    def get_breadcrumbs(self):
+        # Define breadcrumbs as a list of (name, url) tuples
+        return [
+            # ("Home", reverse_lazy("shop_page")),
+            # (self.page_name, None)
+        ]
+
     def get_context_data(self, **kwargs):
         # Check existing attr get_context_data in super()
         gcd = getattr(super(), "get_context_data")
@@ -15,6 +22,7 @@ class BaseContextMixin:
             context = {}
 
         context["page_name"] = self.page_name
+        context["breadcrumbs"] = self.get_breadcrumbs()
 
         return context
 
