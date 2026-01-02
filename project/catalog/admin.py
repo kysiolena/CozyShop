2e98388d0b4855f42be432cf714ec020616464d5
Olena Kysilova <e.a.kiseljova@gmail.com>
2026-01-02 19:05:29 +0200

    Update admin registration in CatalogApp

--- project/catalog/admin.py
+++ project/catalog/admin.py
@@ -1,7 +1,40 @@
 from django.contrib import admin
 
 from catalog.models import Product, Category
+from shop.admin import ImagePreviewMixin
 
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+
+class CategoryAdmin(ImagePreviewMixin, admin.ModelAdmin):
+    fields = [
+        "name",
+        "slug",
+        "description",
+        "image_preview",
+        "image",
+    ]
+
+    # Add image field and the new preview method to readonly_fields
+    readonly_fields = ["image_preview", "created_at", "updated_at"]
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(ImagePreviewMixin, admin.ModelAdmin):
+    fields = [
+        "name",
+        "slug",
+        "description",
+        "price",
+        "sale",
+        "in_stock",
+        "categories",
+        "image_preview",
+        "image",
+    ]
+
+    # Add image field and the new preview method to readonly_fields
+    readonly_fields = ["image_preview", "created_at", "updated_at"]
+
+
+admin.site.register(Product, ProductAdmin)
