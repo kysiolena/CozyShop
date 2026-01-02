cefd9d0c08c2b68dfda607a9e363134685fd121a
Olena Kysilova <e.a.kiseljova@gmail.com>
2026-01-02 19:09:43 +0200

    Add CommentCreateView in CommentApp

--- project/comment/urls.py
+++ project/comment/urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 
 from comment.views import CommentCreateView
 
-urlpatterns = [path("create/", CommentCreateView.as_view(), name="comment_create_page")]
+urlpatterns = [
+    path(
+        "create/<int:product_id>/",
+        CommentCreateView.as_view(),
+        name="comment_create_page",
+    )
+]
