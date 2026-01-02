fc212342daa6215daa4d5231815e9a428212f4c0
Olena Kysilova <e.a.kiseljova@gmail.com>
2026-01-02 19:05:19 +0200

    Update admin registration in CommentApp

--- project/comment/admin.py
+++ project/comment/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 
 from comment.models import Comment
+from shop.admin import ImagePreviewMixin
 
-admin.site.register(Comment)
+
+class CommentAdmin(ImagePreviewMixin, admin.ModelAdmin):
+    fields = ["image_preview", "image", "body", "is_approved", "user", "product"]
+
+    # Add image field and the new preview method to readonly_fields
+    readonly_fields = ["image_preview", "created_at", "updated_at"]
+
+
+admin.site.register(Comment, CommentAdmin)
