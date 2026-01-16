8c3f4303b01b94f2bdf8d0b29a1e4a8acbb19fbe
Olena Kysilova <e.a.kiseljova@gmail.com>
2026-01-16 17:29:35 +0200

    Update start.sh

--- project/start.sh
+++ project/start.sh
@@ -1,3 +1,3 @@
-daphne config.asgi:application -b 0.0.0.0 -p $PORT \
-  & celery -A config worker -E -l info # \
+daphne config.asgi:application -b 0.0.0.0 -p $PORT # \
+  # & celery -A config worker -E -l info \
   # & celery -A config beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
