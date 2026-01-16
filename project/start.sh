cc5a1f2c0727e80eeece05ebd339a66ac9158278
Olena Kysilova <e.a.kiseljova@gmail.com>
2026-01-16 16:13:08 +0200

    Commented out celery beat start command

--- project/start.sh
+++ project/start.sh
@@ -1,1 +1,3 @@
-daphne config.asgi:application -b 0.0.0.0 -p $PORT & celery -A config worker -E -l info & celery -A config beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+daphne config.asgi:application -b 0.0.0.0 -p $PORT \
+  & celery -A config worker -E -l info # \
+  # & celery -A config beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
